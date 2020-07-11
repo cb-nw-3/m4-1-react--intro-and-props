c2208d8316e2fe49a5ec92504a0d8ca41693d3a8
Andres Rueda <garo99@gmail.com>
2020-07-11 16:04:29 -0400

    starting point 3

--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -3,7 +3,14 @@ import React from 'react';
 import './ChatMessage.css';
 
 function ChatMessage(props) {
-  return <div className='chatWrapper'><img className="avatar" alt="avatar" src= {props.message.user.avatar}/> <div className="bodyWrapper"><div className="name">{props.message.user.username}</div><div className="chat-message" >{props.message.body}</div></div></div>
+  return (
+  <div className='chatWrapper'>
+    <img className="avatar" alt="avatar" src= {props.message.user.avatar}/>
+    <div className="bodyWrapper">
+      <div className="name">{props.message.user.username}</div>
+      <div className="chat-message" >{props.message.body}</div>
+    </div>
+  </div>)
 }
 
 export default ChatMessage;
