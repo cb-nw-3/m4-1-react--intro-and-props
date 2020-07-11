3e6f3a3aa034a932767bfc5fe6f6553aff481b2b
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-07-11 14:31:49 -0400

    updated ex3

--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -5,15 +5,25 @@ import './ChatMessage.css';
 function ChatMessage(props) {
     console.log(props);
     let chatBodyClass;
+    let chatAvatarClass;
+    let chatContainerClass;
+    let chatUsernameClass;
+
     if (props.messageType === 'sent') {
         chatBodyClass = 'blue';
+        chatAvatarClass = 'no-avatar';
+        chatContainerClass = 'right';
+        chatUsernameClass ='no-name'
     } else {
         chatBodyClass = 'chat-body';
+        chatAvatarClass = 'chat-avatar';
+        chatContainerClass = 'chat-container';
+        chatUsernameClass ='chat-username';
     }
     return <div className='chat-message'>
-        <img className='chat-avatar' src={props.user.avatar}/>
-        <div className='chat-container'>
-            <div className='chat-username'>{props.user.username}</div>
+        <img className={chatAvatarClass} src={props.user.avatar}/>
+        <div className={chatContainerClass}>
+            <div className={chatUsernameClass}>{props.user.username}</div>
             <div className={chatBodyClass}>{props.body}</div>
         </div>
     </div>
