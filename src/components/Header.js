9f115865b748df4da0082513dda08084c307e679
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-07-12 17:46:52 -0400

    clean-up

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,14 +5,11 @@ import "./Header.css";
 function Header(props) {
   console.log(props);
   // console.log(props.currentUser);
-
   return <header>{props.participants.map(participant => {
-    let participantAvatar = participant.avatar;
     if (participant !== props.currentUser) {
       return (
-          
           <div className='header-container'>
-            <img className='header-avatar' src={participantAvatar} />
+            <img className='header-avatar' src={participant.avatar} />
             <div className='header-username'>{participant.username}</div>
           </div>
       )
