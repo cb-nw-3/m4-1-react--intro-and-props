a78cf2868b9047b5fca6cdd07f46ba460d7da368
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-07-12 14:15:43 -0400

    added respective tips stretch goal

--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -8,23 +8,29 @@ function ChatMessage(props) {
     let chatAvatarClass;
     let chatContainerClass;
     let chatUsernameClass;
-
+    let tipClass;
+    let tipSrc;
     if (props.messageType === 'sent') {
         chatBodyClass = 'blue';
         chatAvatarClass = 'no-avatar';
         chatContainerClass = 'right';
-        chatUsernameClass ='no-name'
+        chatUsernameClass ='no-name';
+        tipClass = 'tip-sent';
+        tipSrc = '/assets/tip-sent.svg';
     } else {
         chatBodyClass = 'chat-body';
         chatAvatarClass = 'chat-avatar';
         chatContainerClass = 'chat-container';
         chatUsernameClass ='chat-username';
+        tipClass = 'tip-received';
+        tipSrc = '/assets/tip-received.svg';
     }
     return <div className='chat-message'>
         <img className={chatAvatarClass} src={props.user.avatar}/>
         <div className={chatContainerClass}>
             <div className={chatUsernameClass}>{props.user.username}</div>
             <div className={chatBodyClass}>{props.body}</div>
+            <div><img className={tipClass} src={tipSrc} /></div>
         </div>
     </div>
 }
