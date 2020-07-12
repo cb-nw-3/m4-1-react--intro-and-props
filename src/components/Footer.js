4359787f850b2659126010852e1047e00f661c10
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-07-12 17:11:50 -0400

    added text input stretch goal

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,9 @@ import React from "react";
 import "./Footer.css";
 
 function Footer(props) {
-  return <footer>{/* Your code here! */}</footer>;
+return <footer>{<div className='text-box'>
+  <input className='text-input' type='text'></input>
+  </div>}</footer>;
 }
 
 export default Footer;
