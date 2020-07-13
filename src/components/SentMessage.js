9b15de325b8479183934888db8ce839814ae0fb0
Andres Rueda <garo99@gmail.com>
2020-07-12 21:24:50 -0400

    chat bubbles pure css

--- src/components/SentMessage.js
+++ src/components/SentMessage.js
@@ -4,9 +4,7 @@ function SentMessage(props) {
     console.log(props)
     return (
         <div className='chatWrapperSent'>
-          <img className="avatar" alt="avatar" src= {props.message.user.avatar}/>
           <div className="bodyWrapper">
-            <div className="name">{props.message.user.username}</div>
             <div className="chat-message-sent" >{props.message.body}</div>
           </div>
         </div>)
