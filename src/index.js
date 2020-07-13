2021c1580fdcad8ec75b5244b3aea4f7e23f6f1d
Andres Rueda <garo99@gmail.com>
2020-07-13 15:28:32 -0400

    css revised

--- src/index.js
+++ src/index.js
@@ -12,15 +12,18 @@ const sideA = document.createElement('div');
 const sideB = document.createElement('div');
 wrapperAll.classList.add('wrapperAll');
 rootElement.appendChild(wrapperAll);
+sideA.classList.add("sideA")
+sideB.classList.add("sideB")
+
 wrapperAll.appendChild(sideA);
 wrapperAll.appendChild(sideB);
 
 
-console.log(data.currentUser);
 ReactDOM.render(
   <App currentUser={data.currentUser} conversation={data.conversation} />,
   sideA
 );
+
 ReactDOM.render(
   <App currentUser= {data.currentUser2} conversation={data.conversation} />,
   sideB
