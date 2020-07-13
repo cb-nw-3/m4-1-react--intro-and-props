12aa44944845e1e08f3278e32fcfaaa632ea70e9
Przemek Piwek <przemekpiwek@gmail.com>
2020-07-12 22:11:41 -0600

    Stretch Footer Complete

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,13 @@ import React from "react";
 import "./Footer.css";
 
 function Footer(props) {
-  return <footer>{/* Your code here! */}</footer>;
+  return (
+    <footer>
+      <div className="footerContainer">
+        <input className="textBox" />
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
