3bb994482f3d4b505e0f5f8e17891fefdbd35dd8
Andres Rueda <garo99@gmail.com>
2020-07-13 13:15:08 -0400

    side by side

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,6 @@ import React from "react";
 
 import "./Footer.css";
 function Footer(props) {
-  console.log("footer")
   return (<footer><input className="textInput"></input></footer>);
 }
 
--- src/index.js
+++ src/index.js
@@ -7,8 +7,23 @@ import App from './components/App';
 import './styles.css';
 
 const rootElement = document.getElementById('root');
+const wrapperAll = document.createElement('div');
+const sideA = document.createElement('div');
+const sideB = document.createElement('div');
+wrapperAll.classList.add('wrapperAll');
+rootElement.appendChild(wrapperAll);
+wrapperAll.appendChild(sideA);
+wrapperAll.appendChild(sideB);
 
+
+console.log(data.currentUser);
 ReactDOM.render(
   <App currentUser={data.currentUser} conversation={data.conversation} />,
-  rootElement
+  sideA
 );
+ReactDOM.render(
+  <App currentUser= {data.currentUser2} conversation={data.conversation} />,
+  sideB
+);
+
+
