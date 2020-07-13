220832912db8609c25b4feb731aace089f35e271
Thinh Nguyen <thinh-nguyen@hotmail.com>
2020-07-13 17:29:42 -0400

    rendered individual chat msgs

--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -3,7 +3,13 @@ import React from 'react';
 import './ChatMessage.css';
 
 function ChatMessage(props) {
-  return <div className="chat-message"></div>;
+  return (
+    <div className="chat-message">
+      <img src={props.message.user.avatar} />
+      <h1>{props.message.user.username}</h1>
+      <p>{props.message.body}</p>
+    </div>
+    );
 }
 
 export default ChatMessage;
