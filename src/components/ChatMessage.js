092990bdcb32e2cf11a877bf5010e676f053194c
Thinh Nguyen <thinh-nguyen@hotmail.com>
2020-07-13 21:00:41 -0400

    added bubble tips

--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -2,10 +2,14 @@ import React from 'react';
 
 import './ChatMessage.css';
 
+const bubbleTipSentSrc = '/assets/tip-sent.svg';
+const bubbleTipReceivedSrc = '/assets/tip-received.svg';
+
 function SentMessage(props) {
   return (
     <div className="chat-message chat-message--sent">
       {/* <img src={props.message.user.avatar} /> */}
+      <img className ='msg-tip' src={bubbleTipSentSrc}/>
       <div className="chat-message__content chat-message__content--sent">
         {/* <h1>{props.message.user.username}</h1> */}
         <p>{props.message.body}</p>
@@ -18,10 +22,15 @@ function ReceivedMessage(props) {
   return (
     <div className="chat-message">
       <img src={props.message.user.avatar} />
+
       <div className="chat-message__content chat-message__content--recvd">
         <h1>{props.message.user.username}</h1>
-        <p>{props.message.body}</p>
+        <div className="chat-message__content__inside">
+          <p>{props.message.body}</p>
+          <img className ='msg-tip--recvd' src={bubbleTipReceivedSrc}/>
+        </div>
       </div>
+
     </div>
     );
 }
