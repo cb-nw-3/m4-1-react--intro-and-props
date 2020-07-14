4687d3fb66acb38151713290115dc29f967ebff7
Mae <maeberris@outlook.com>
2020-07-13 22:36:00 -0400

    added strecth goal 1

--- src/components/SentMessage.js
+++ src/components/SentMessage.js
@@ -6,6 +6,7 @@ function SentMessage(props) {
   return (
     <div className="chat-message-sent">
       <div className="nameAndMessage-sent">
+        <img className="sentTip" src="/assets/tip-sent.svg" />
         <div className="message-sent">{props.message.body}</div>
       </div>
     </div>
--- src/components/receivedMessage.js
+++ src/components/receivedMessage.js
@@ -7,6 +7,7 @@ function ReceivedMessage(props) {
     <div className="chat-message">
       <img alt="profile" src={props.message.user.avatar} />
       <div className="nameAndMessage">
+        <img className="receivedTip" src="/assets/tip-received.svg" />
         <div className="name">{props.message.user.username}</div>
         <div className="message">{props.message.body}</div>
       </div>
