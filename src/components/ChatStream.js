b131051a19e941b5a1b3d0d7aa6766795e5dba77
Truong Dao <truong.dao.do@gmail.com>
2020-07-18 11:52:33 -0400

    Soluition_React1_TD

--- src/components/ChatStream.js
+++ src/components/ChatStream.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import './ChatStream.css';
 
+import ChatMessage from './ChatMessages';
+
 // The current user's messages should have:
 // - a text color of #FFF
 // - a background color of #1185f7
@@ -11,7 +13,31 @@ import './ChatStream.css';
 // - a background color of #e9e9eb
 
 function ChatStream(props) {
-  return <section className="chat-stream">{/* Your code here! */}</section>;
+  // console.log(props);
+  return (
+    <section className="chat-stream">
+      {/* Maps over the each message from the selected object in a <div> tag. */}
+      {props.messages.map((message) => {
+        // return <div>{message.body}</div>; // No longer needed from part 1.
+        // return <ChatMessage message={message} />; // No longer needed from part 2.
+        let messageType;
+        if (message.user.username === props.currentUser) {
+          messageType = 'sent';
+        } else {
+          messageType = 'received';
+        }
+        // console.log(messageType);
+        return (
+          <ChatMessage
+            message={message} // Added for Ex 3.
+            // user={message.user} // Commented out for Ex 3.
+            // body={message.body} // COmmented out for Ex 3.
+            messageType={messageType}
+          />
+        );
+      })}
+    </section>
+  );
 }
 
 export default ChatStream;
